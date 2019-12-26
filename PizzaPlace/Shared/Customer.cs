62a7a688bc1f065f8c173705044f9412fef8561c
Nejc Brelih-Wasowski <nejcbw@protonmail.com>
2019-12-26 11:57:23 +0100

    End of chapter 4

---
+++ PizzaPlace/Shared/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.ComponentModel;
+
+namespace PizzaPlace.Shared
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ PizzaPlace/Shared/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PizzaPlace.Shared
+{
+    public class Menu
+    {
+        public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+        public Pizza GetPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
